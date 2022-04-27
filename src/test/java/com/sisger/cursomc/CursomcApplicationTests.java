bdea2adb5984e65ccb0861a085274200998a0d81
Eugenio <eugenio@sisger.com.br>
2022-04-27 19:38:51 -0300

    Projeto criado

---
+++ src/test/java/com/sisger/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisger.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
