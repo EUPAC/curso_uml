3ed42ae2afb38e21e986dcc46cbcd94ab00df68b
Eugenio <eugenio@sisger.com.br>
2022-04-29 07:57:25 -0300

    Produtos e associação muitos para muitos

---
+++ src/main/java/com/sisger/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.sisger.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sisger.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+}
